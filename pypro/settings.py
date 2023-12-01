f974fa2c267924598407647e815bb0773c4da538
Davidson <deivisonj1@hotmail.com>
2023-12-01 20:17:10 -0300

    bando de dados

--- pypro/settings.py
+++ pypro/settings.py
@@ -178,6 +178,7 @@ if AWS_ACCESS_KEY_ID:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
